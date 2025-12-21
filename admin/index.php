e51e730dba0ccdaf4e55b7abe9bfac315469107d
Aslı <asli.koturoglu@sabanciuniv.edu>
2025-12-21 18:59:35 +0300

    admin panel can be seen

--- admin/index.php
+++ admin/index.php
@@ -12,3 +12,23 @@ $query = new MongoDB\Driver\Query([
 
 $cursor = $manager->executeQuery("cs306.tickets", $query);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Panel</title>
+</head>
+<body>
+
+<h2>Admin Ticket List</h2>
+
+<?php foreach ($cursor as $ticket): ?>
+    <div style="border:1px solid red; padding:10px; margin:10px;">
+        <b>User:</b> <?= $ticket->username ?><br>
+        <b>Message:</b> <?= $ticket->message ?><br>
+        <b>Status:</b> <?= $ticket->status ? "Open" : "Closed" ?><br>
+        <b>Created:</b> <?= $ticket->created_at ?><br>
+    </div>
+<?php endforeach; ?>
+
+</body>
+</html>
